c6bf29ea14b4ff3d9baa2d11d948185878c5ad3b
kappanneo <pannacciclaudio@gmail.com>
2022-03-27 22:56:25 +0200

    a bunch of stuff

--- Card.hs
+++ Card.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE MultiParamTypeClasses, AllowAmbiguousTypes #-}
-
 module Card where
 import Number 
 
@@ -11,25 +9,29 @@ data Card = Card Color Number
 
 type Deck = Bunch Card
 
-data Bunch a = Nope | Bunch a a
+infixl 5 `Comma`
+infixl 6 `And`
+
+data Bunch a = a `Comma` (Bunch a) |  a `And` a
  deriving (Eq, Show, Read)
 
+bunch :: a -> a -> [a] -> Bunch a
+bunch c0 c1 [] = c0 `And` c1
+bunch c0 c1 (c2:cs) = c0 `Comma` bunch c1 c2 cs
 
-class Fuck a b where
+--- TEST
 
-   (#@!) :: a -> b -> b
-   nope :: b
+c0 = Card White Zero
+c1 = Card Red (toEnum 100)
+c2 = Card Yellow (toEnum 42)
+c3 = Card Red (toEnum 4)
+c4 = Card Green (Succ Zero)
+c5 = Card Blue (Succ (Succ Zero))
+c6 = Card Blue Zero
+c7 = Card Green Zero
 
-class Fuck a b => FFS a b where
+d0 = c0 `And` c1
+d1 = c5 `Comma` (c6 `Comma` (c3 `And` c4))
+d2 = c2 `And` c7
 
-   ffs :: Fuck a b => [a] -> b
-   ffs [] = nope :: b
-   ffs (x:xs) = x #@! (ffs xs)  
-
-
-
-
-
-
-
-
+deck = bunch c0 c1 [c2,c3,c4,c5,c6,c7]
